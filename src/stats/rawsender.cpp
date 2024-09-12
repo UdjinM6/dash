03339ba8dd29e2d18deaa7138477c145ff5e545b
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-09-12 15:28:37 +0000

    feat(stats): introduce support for queuing messages

--- src/stats/rawsender.cpp
+++ src/stats/rawsender.cpp
@@ -8,10 +8,13 @@
 #include <netaddress.h>
 #include <netbase.h>
 #include <util/sock.h>
+#include <util/thread.h>
 
-RawSender::RawSender(const std::string& host, uint16_t port, std::optional<std::string>& error) noexcept :
+RawSender::RawSender(const std::string& host, uint16_t port, uint64_t interval_ms,
+                     std::optional<std::string>& error) noexcept :
     m_host{host},
-    m_port{port}
+    m_port{port},
+    m_interval_ms{interval_ms}
 {
     if (host.empty()) {
         error = "No host specified";
@@ -39,17 +42,44 @@ RawSender::RawSender(const std::string& host, uint16_t port, std::optional<std::
     }
     m_sock = std::make_unique<Sock>(hSocket);
 
-    LogPrintf("Started RawSender sending messages to %s:%d\n", m_host, m_port);
+    if (m_interval_ms == 0) {
+        LogPrintf("Send interval is zero, not starting RawSender queueing thread.\n");
+    } else {
+        m_interrupt.reset();
+        m_thread = std::thread(&util::TraceThread, "rawsender", [this] { QueueThreadMain(); });
+    }
+
+    LogPrintf("Started %sRawSender sending messages to %s:%d\n", m_thread.joinable() ? "threaded " : "", m_host, m_port);
 }
 
 RawSender::~RawSender()
 {
-    LogPrintf("Stopping RawSender instance sending messages to %s:%d. %d successes, %d failures.\n",
-              m_host, m_port, m_successes, m_failures);
+    // If there is a thread, interrupt and stop it
+    if (m_thread.joinable()) {
+        m_interrupt();
+        m_thread.join();
+    }
+    // Flush queue of uncommitted messages
+    QueueFlush();
+    // Reset connection socket
     m_sock.reset();
+
+    LogPrintf("Stopped RawSender instance sending messages to %s:%d. %d successes, %d failures.\n",
+              m_host, m_port, m_successes, m_failures);
 }
 
 std::optional<std::string> RawSender::Send(const RawMessage& msg) noexcept
+{
+    // If there is a thread, append to queue
+    if (m_thread.joinable()) {
+        QueueAdd(msg);
+        return std::nullopt;
+    }
+    // There isn't a queue, send directly
+    return SendDirectly(msg);
+}
+
+std::optional<std::string> RawSender::SendDirectly(const RawMessage& msg) noexcept
 {
     if (!m_sock) {
         m_failures++;
@@ -73,3 +103,42 @@ std::optional<std::string> RawSender::Send(const RawMessage& msg) noexcept
 }
 
 std::string RawSender::ToStringHostPort() const noexcept { return strprintf("%s:%d", m_host, m_port); }
+
+void RawSender::QueueAdd(const RawMessage& msg) noexcept
+{
+    AssertLockNotHeld(cs);
+    WITH_LOCK(cs, m_queue.push_back(msg));
+}
+
+void RawSender::QueueFlush() noexcept
+{
+    AssertLockNotHeld(cs);
+    WITH_LOCK(cs, QueueFlush(m_queue));
+}
+
+void RawSender::QueueFlush(std::deque<RawMessage>& queue) noexcept
+{
+    while (!queue.empty()) {
+        SendDirectly(queue.front());
+        queue.pop_front();
+    }
+}
+
+void RawSender::QueueThreadMain() noexcept
+{
+    AssertLockNotHeld(cs);
+
+    while (!m_interrupt) {
+        // Swap the queues to commit the existing queue of messages
+        std::deque<RawMessage> queue;
+        WITH_LOCK(cs, m_queue.swap(queue));
+
+        // Flush the committed queue
+        QueueFlush(queue);
+        assert(queue.empty());
+
+        if (!m_interrupt.sleep_for(std::chrono::milliseconds(m_interval_ms))) {
+            return;
+        }
+    }
+}
