2d9fc47d80722caa36db227012a4e4c8495005c9
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-12-01 01:53:55 +0530

    refactor: adapt CbTx, CreditPool logic to `DeploymentActive{At,After}`

--- src/evo/specialtxman.cpp
+++ src/evo/specialtxman.cpp
@@ -559,7 +559,7 @@ bool CSpecialTxProcessor::ProcessSpecialTxsInBlock(const CBlock& block, const CB
 
         CRangesSet indexes;
         if (pindex->nHeight >= m_consensus_params.DeploymentHeight(Consensus::DEPLOYMENT_V20)) {
-            CCreditPool creditPool{m_cpoolman.GetCreditPool(pindex->pprev, m_consensus_params)};
+            CCreditPool creditPool{m_cpoolman.GetCreditPool(pindex->pprev)};
             LogPrint(BCLog::CREDITPOOL, "CSpecialTxProcessor::%s -- CCreditPool is %s\n", __func__, creditPool.ToString());
             indexes = std::move(creditPool.indexes);
         }
