dcf37c1f98d18bc2ca92bbee233f68b1aba36289
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-10 19:45:29 +0530

    refactor: use `std23::ranges::contains` in Dash-specific code

--- src/active/dkgsession.cpp
+++ src/active/dkgsession.cpp
@@ -13,6 +13,7 @@
 #include <llmq/utils.h>
 #include <masternode/meta.h>
 #include <util/ranges.h>
+#include <util/std23.h>
 
 #include <chain.h>
 #include <deploymentstatus.h>
@@ -355,7 +356,7 @@ void ActiveDKGSession::SendJustification(CDKGPendingMessages& pendingMessages, P
 
     for (const uint32_t i : util::irange(members.size())) {
         const auto& m = members[i];
-        if (forMembers.count(m->dmn->proTxHash) == 0) {
+        if (!std23::ranges::contains(forMembers, m->dmn->proTxHash)) {
             continue;
         }
         logger.Batch("justifying for %s", m->dmn->proTxHash.ToString());
@@ -560,7 +561,7 @@ std::vector<CFinalCommitment> ActiveDKGSession::FinalizeCommitments()
 
         for (const auto& p : prematureCommitments) {
             const auto& qc = p.second;
-            if (validCommitments.count(p.first) == 0) {
+            if (!std23::ranges::contains(validCommitments, p.first)) {
                 continue;
             }
 
