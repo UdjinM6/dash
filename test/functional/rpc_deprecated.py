00d18f80d29c06937270fa02b9a44f7d49fd61ec
Konstantin Akimov <knstqq@gmail.com>
2026-03-24 14:46:44 +0700

    test: add functional tests for -deprecatedrpc=permissive_bool

--- test/functional/rpc_deprecated.py
+++ test/functional/rpc_deprecated.py
@@ -4,15 +4,16 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test deprecation of RPC calls."""
 from test_framework.test_framework import BitcoinTestFramework
-# from test_framework.util import assert_raises_rpc_error
+from test_framework.util import assert_raises_rpc_error, assert_equal
 
 class DeprecatedRpcTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
-        self.extra_args = [[], []]
+        self.extra_args = [[], ["-deprecatedrpc=permissive_bool"]]
 
     def run_test(self):
+        self.test_permissive_bool()
         # This test should be used to verify correct behaviour of deprecated
         # RPC methods with and without the -deprecatedrpc flags. For example:
         #
@@ -24,6 +25,18 @@ class DeprecatedRpcTest(BitcoinTestFramework):
         # assert_raises_rpc_error(-32, 'The wallet generate rpc method is deprecated', self.nodes[0].rpc.generate, 1)
         # self.generate(self.nodes[1], 1)
         self.log.info("No tested deprecated RPC methods")
+
+
+    def test_permissive_bool(self):
+        self.generate(self.nodes[0], 1)
+        assert_equal([], self.nodes[0].protx("list", "valid", True))
+        assert_raises_rpc_error(-8, 'detailed must be a JSON boolean. Pass -deprecatedrpc=permissive_bool to allow legacy boolean parsing.', self.nodes[0].protx, "list", "valid", 1)
+        assert_raises_rpc_error(-8, 'detailed must be a JSON boolean. Pass -deprecatedrpc=permissive_bool to allow legacy boolean parsing.', self.nodes[0].protx, "list", "valid", "1")
+
+        assert_equal([], self.nodes[1].protx("list", "valid", True))
+        assert_equal([], self.nodes[1].protx("list", "valid", 1))
+        assert_equal([], self.nodes[1].protx("list", "valid", "1"))
+
 
 if __name__ == '__main__':
     DeprecatedRpcTest().main()
