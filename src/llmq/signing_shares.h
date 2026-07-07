2a2bc9d2251386b56d4426cd8ff81ec3fb26ee2d
PastaClaw <thepastaclaw@users.noreply.github.com>
2026-07-07 18:11:11 -0500

    llmq: bound batched sig-share intake

--- src/llmq/signing_shares.h
+++ src/llmq/signing_shares.h
@@ -154,7 +154,22 @@ public:
 public:
     SERIALIZE_METHODS(CBatchedSigShares, obj)
     {
-        READWRITE(VARINT(obj.sessionId), obj.sigShares);
+        READWRITE(VARINT(obj.sessionId));
+        if constexpr (ser_action.ForRead()) {
+            const uint64_t sig_shares_size{ReadCompactSize(s)};
+            if (sig_shares_size > MAX_MSGS_TOTAL_BATCHED_SIGS) {
+                throw std::ios_base::failure("CBatchedSigShares::sigShares size too large");
+            }
+            const size_t limited_sig_shares_size{static_cast<size_t>(sig_shares_size)};
+            obj.sigShares.clear();
+            obj.sigShares.reserve(limited_sig_shares_size);
+            while (obj.sigShares.size() < limited_sig_shares_size) {
+                obj.sigShares.emplace_back();
+                READWRITE(obj.sigShares.back());
+            }
+        } else {
+            READWRITE(obj.sigShares);
+        }
     }
 
     [[nodiscard]] std::string ToInvString() const;
